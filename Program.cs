0763d392fba5681d412a2b221b39e57543c46cc0
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-10-23 14:50:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OOPsDemo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Create classes in library
+       Computers comp=new Computers();
+        comp.ProductId = 101;
+        comp.ProductName = "Laptop";
+        comp.RAMCapacity = 16;
+        comp.HDD = 25;
+        Accessories accessories = new Accessories();    
+        accessories.MousePad = true;
+        accessories.Mouse = true;
+        accessories.Bag = true;
+        accessories.HDMICable = true;   
+        comp.GetAccessories = accessories;
+       
+       
+        
+
+        Cart cart=new Cart();
+        double amt=cart.AddToCart(comp);
+        Console.WriteLine($"Total cost= {amt}");
+
+
+
+
+
+
+    }
+}
